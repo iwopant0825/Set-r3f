0ca674539f7c80d9ce88e7fd4a05865426438118
iwopant0825 <129718228+iwopant0825@users.noreply.github.com>
2024-04-22 10:50:58 +0900

    불사조 카드 개발

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,34 @@
+import { Environment, MeshPortalMaterial, OrbitControls, RoundedBox, Sky, useTexture } from "@react-three/drei"
 import { Canvas } from "@react-three/fiber"
+import * as THREE from 'three'
+import { Model } from "./model"
 
 function App() {
   return (
     <>
       <Canvas>
-        
+        <RoundedBox args={[2,3,0.1]} >
+          <MeshPortalMaterial side={THREE.DoubleSide}>
+            <Model scale={0.0024}/>
+            <Back/>
+          </MeshPortalMaterial>
+        </RoundedBox>
       </Canvas>
+    </>
+  )
+}
+
+function Back(){
+  const map = useTexture("/back.jpg")
+  return(
+    <>
+      <ambientLight intensity={1}/>
+      <Environment preset="sunset"/>
+      <OrbitControls/>
+      <mesh>
+        <sphereGeometry args={[5,64,64]}/>
+        <meshStandardMaterial map={map} side={THREE.BackSide}/>
+      </mesh>
     </>
   )
 }
